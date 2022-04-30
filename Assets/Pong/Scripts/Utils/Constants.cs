a5f37ffb6b18d3a874950368cfc65fd8c5ced14a
Shahzaib <shahzaib52@hotmail.com>
2022-04-30 23:39:18 +0500

    disappearance bug fix

--- Assets/Pong/Scripts/Utils/Constants.cs
+++ Assets/Pong/Scripts/Utils/Constants.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public static class Constants
 {
-	public static readonly Vector2 PLAYER = new Vector2(8, -0.25f);
-	public static readonly Vector2 AI = new Vector2(-8, -0.25f);
-	public static readonly Vector3 PADDLE_SCALE = new Vector3(1f, 1f, 1f);
-	public static readonly float PADDLE_SPEED = 15f;
-	public static readonly float PADDLE_SPEED_AI = 5f;
+    public static readonly Vector2 PLAYER = new Vector3(8, 10, -10);
+    public static readonly Vector2 AI = new Vector3(-8, 0, -10);
+    public static readonly Vector3 PADDLE_SCALE = new Vector3(1f, 1f, 1f);
+    public static readonly float PADDLE_SPEED = 15f;
+    public static readonly float PADDLE_SPEED_AI = 5f;
 
     public static string APP_VERSION = "V1.0";
     public static string WalletAccountKey = "Account";
@@ -21,20 +21,20 @@ public static class Constants
     public static bool TournamentActive = false;
 
     public static bool IsTest = false; //bool to test wallet functionality inside editor
-	public static bool IsTestNet = false;//bool to test code on testnet chain
+    public static bool IsTestNet = false;//bool to test code on testnet chain
     public static bool IsStagging = true;
     public static bool IsDebugBuild = false;
 
 
     public static string UserName = "";//name of the user
-	public static int FlagSelectedIndex = 0;//flag selected index for avatar
-	public static int TotalWins = 0; //variable to store total wins of the player
+    public static int FlagSelectedIndex = 0;//flag selected index for avatar
+    public static int TotalWins = 0; //variable to store total wins of the player
     public static int TotalHits = 0;//variable to store total hits of player
-	public static int TotalScore = 0; //variable to store total score accomulation of the user
-	public static string WalletAddress = ""; //stored wallet address for the connected user
-	public static bool WalletConnected = false;//bool to keep track if wallet was connected or not
-	public static bool WalletChanged = false;//bool to keep track if wallet was changed
-	public static bool isUsingFirebaseSDK = false;//switch to enable/disable firebase sdk usage
+    public static int TotalScore = 0; //variable to store total score accomulation of the user
+    public static string WalletAddress = ""; //stored wallet address for the connected user
+    public static bool WalletConnected = false;//bool to keep track if wallet was connected or not
+    public static bool WalletChanged = false;//bool to keep track if wallet was changed
+    public static bool isUsingFirebaseSDK = false;//switch to enable/disable firebase sdk usage
 
     public static bool LoggedIn = false; //keep track of loggin from firebase
     public static bool RegisterSubmit = false;
@@ -60,9 +60,9 @@ public static class Constants
     public static int LeaderboardCount = 200;//top player count for leaderboard
 
     public static void PrintLog(string Txt)
-	{
-		Debug.Log(Txt);
-	}
+    {
+        Debug.Log(Txt);
+    }
 
     public static void PrintError(string Txt)
     {
@@ -71,7 +71,7 @@ public static class Constants
 
     public static void PrintExp(Exception Txt, UnityEngine.Object ins)
     {
-        Debug.LogException(Txt,ins);
+        Debug.LogException(Txt, ins);
     }
     public static void ResetData()
     {
