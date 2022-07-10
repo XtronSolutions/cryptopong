9b09915b5553ad941c85f60527fdd0d144052283
Shahzaib <shahzaib52@hotmail.com>
2022-07-11 00:17:43 +0500

    all fixed

--- Assets/WebglPreBuildProcessing/WebglPreBuildProcessing.cs
+++ Assets/WebglPreBuildProcessing/WebglPreBuildProcessing.cs
@@ -7,7 +7,7 @@ public class WebglPreBuildProcessing : IPreprocessBuildWithReport
 public int callbackOrder => 1;
 public void OnPreprocessBuild(BuildReport report)
 {
-System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/3.10/bin/python");
+System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7");
 }
 }
 #endif
